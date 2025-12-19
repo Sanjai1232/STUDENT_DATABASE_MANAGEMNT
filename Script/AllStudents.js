9624318fb4c4f399c8eab806b4cd7c1404ae8c49
Sanjai1232 <sanjaisanjai6551@gmail.com>
2025-12-19 18:43:31 +0530

    make some minor changes in All student module

--- Script/AllStudents.js
+++ Script/AllStudents.js
@@ -1,3 +1,9 @@
+
+async function loadData() {
+    const token = localStorage.getItem('token');
+    if (!token) return window.location.href = "login.html";
+}
+loadData()
 const funct=async()=>{
    const res=await fetch("http://localhost:3000/All_Student",{
     method:'GET',
@@ -11,17 +17,19 @@ const funct=async()=>{
     const data=await res.json();
   //  console.log(data)
 
-  const ssname=document.getElementById('stuname');
-  const ssrollno=document.getElementById('sturollno');
+  const ssname=document.getElementById('n');
+  const ssrollno=document.getElementById('rno');
   const rr=data.data;
+  console.log(data);
+  
   console.log(rr);
-    ssname.innerText="";
-    rr.forEach(element => {
-      
-        ssname.innerHTML+=`name: <P>${element.name} Rollno: ${element.Rollno}</P> <br>`;
-        // ssrollno.innerHTML+=`<h4>${element.rollno}</h4><br>`;
-    }
-    )   ;
+  let table = document.getElementById("myTable");
+  rr.forEach(student => {
+    let row = table.insertRow();
+    row.insertCell(0).innerHTML = student.name;
+     row.insertCell(1).innerHTML = student.department;
+  });
+
     
 }
 funct();
